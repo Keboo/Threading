750c44669cff859885980782aaf261ce306ffc89
Kevin Bost <kitokeboo@gmail.com>
2017-10-31 07:57:30 -0700

    Adding SizeOfThread.

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Threading
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
+            SizeOfThread();
         }
     }
 }
---
+++ Threading/SizeOfThread.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Threading
+{
+    partial class Program
+    {
+        public static void SizeOfThread()
+        {
+            const int MB = 1024 * 1024;
+            do
+            {
+                Thread t = new Thread(RunForever);
+                t.Start();
+                Console.WriteLine($"Current memory size {Process.GetCurrentProcess().VirtualMemorySize64 / MB} MB");
+            } while (Console.ReadLine() == "");
+        }
+
+        private static void RunForever()
+        {
+            while(true) { }
+        }
+    }
+}
