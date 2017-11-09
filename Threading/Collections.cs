cf78ea5d0a699cd81dc6c02bdfb288643fa415aa
Kevin Bost <kitokeboo@gmail.com>
2017-11-08 22:51:02 -0800

    Adding Collections.cs

---
+++ Threading/Collections.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Threading
+{
+    partial class Program
+    {
+        public static void Collections()
+        {
+            var bag = new ConcurrentBag<int>();
+            var dictionary = new ConcurrentDictionary<int, string>();
+            var queue = new ConcurrentQueue<int>();
+            var stack = new ConcurrentStack<int>();
+            
+        }
+    }
+}
