c0b69cd59fbc42a0edd00f4a901f02376b838bfb
Kevin Bost <kitokeboo@gmail.com>
2017-11-09 14:37:53 -0800

    Adding atomic operations

---
+++ Threading/Atomic.cs
@@ -0,0 +1,19 @@
+﻿namespace Threading
+{
+    partial class Program
+    {
+        private static int _Int;
+        private static long _Long;
+        private static string _String;
+
+        public static void AtomicOperations()
+        {
+            _Int = 42;
+            _Int++;
+            _Long = 42L;
+            _Long ^= _Long;
+
+
+        }
+    }
+}
